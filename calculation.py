24451f63f16e88afee9f270d688133dd7e189ce0
monirjihad <monirhossainjihad@gmail.com>
2023-07-27 22:06:42 +0600

    eleventh version (in past, we used English numbers(0,1,2,..) in calculation.py. But now, we can use Bangla numbers and show Bangla result in 1.calculation mode )

--- calculation.py
+++ calculation.py
@@ -2,6 +2,7 @@ import cv2
 import mediapipe as mp
 from keys import *
 import main as ma
+from PIL import ImageFont, ImageDraw, Image
 
 class Calculator:
     def __init__(self):
@@ -12,9 +13,12 @@ class Calculator:
         self.x = []
         self.y = []
         self.text = ""
+        self.textB = ""
+        self.ban = ""
         self.k = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0]
         self.idset = ["", "1", "12", "123", "1234", "01234", "0", "01", "012", "0123", "04", "124", "134", "014", "14", "234", "4"]
         self.op = ["", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "+", "-", "*", "/"]
+        self.opB = ["", "১", "২", "৩", "৪", "৫", "৬", "৭", "৮", "৯", "০", "+", "-", "*", "/"]
 
         # copied from main.py to ensure same frame-size & show button
         self.calculationKey = Key(50, 5, 300, 50, '1.Calculation')
@@ -23,6 +27,31 @@ class Calculator:
         self.frameHeight, self.frameWidth, _ = self.cam.read()[1].shape
         self.calculationKey.x = int(self.frameWidth * .73) - 150
         self.backKey.x = int(self.frameWidth * 1.4) - 150  # To add "back" button
+
+    def num_to_str(self, argument):
+        switcher = {
+            0: "০",
+            1: "১",
+            2: "২",
+            3: "৩",
+            4: "৪",
+            5: "৫",
+            6: "৬",
+            7: "৭",
+            8: "৮",
+            9: "৯",
+        }
+        return switcher.get(argument)
+    def eng_to_ban(self,text):
+       for i in range(len(text)):
+           if text[i] == "-" or text[i] == "." :
+               self.ban = self.ban + text[i]
+           else:
+             a = int(text[i])
+             b = self.num_to_str(a)
+             self.ban = self.ban + b
+       return self.ban
+
 
     def calculation_frame(self):
         while True:
@@ -73,21 +102,31 @@ class Calculator:
                                 if self.k[i] > 20:
                                     if i == 15:
                                         ans = str(eval(self.text))
-                                        self.text = self.text + " = " + ans
+                                        ans = self.eng_to_ban(ans)
+                                        self.ban = ""
+                                        self.textB = self.textB + " = " + ans
                                         for i in range(len(self.k)):
                                             self.k[i] = 0
                                     elif i == 16:
                                         main = ma.Main()
                                         main.process_frame()
                                     else:
+                                        self.textB += self.opB[i]
                                         self.text += self.op[i]
                                         for i in range(len(self.k)):
                                             self.k[i] = 0
 
-                    cv2.putText(imgg, self.text, (100, 120), cv2.FONT_HERSHEY_TRIPLEX, 2, (255, 0, 0), 3)
+                    #cv2.putText(imgg, self.textB, (100, 120), cv2.FONT_HERSHEY_TRIPLEX, 2, (255, 0, 0), 3)
+                    fontpath = "fonts/Kalpurush-Regular.ttf"
+                    font = ImageFont.truetype(fontpath, 70)
+                    img_pil = Image.fromarray(imgg)
+                    draw = ImageDraw.Draw(img_pil)
+                    draw.text((100, 100), self.textB, font=font, fill=(0, 0, 255, 0))
+                    imgg = np.array(img_pil)
                     self.mpDraw.draw_landmarks(imgg, handLms, self.mpHands.HAND_CONNECTIONS)
             else:
                 self.text = " "
+                self.textB = " "
 
             cv2.imshow("WebCam", imgg)
             cv2.waitKey(1)
